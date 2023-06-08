8c787b97c328a03dd963b59da3aa10ac750b5a5b
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-06-08 13:54:40 +0300

    feat(users_create): return the access and  refresh token when user is created

--- users/api.py
+++ users/api.py
@@ -2,6 +2,7 @@
 from django.core.handlers.wsgi import WSGIRequest
 from ninja import Router
 from ninja_jwt.authentication import JWTAuth
+from ninja_jwt.tokens import RefreshToken
 
 from .models import CustomUser
 from .schema import UserSchema
@@ -9,13 +10,22 @@ from .schema import UserSchema
 router = Router()
 
 
-@router.post("/", response=UserSchema)
-def create(request: WSGIRequest, payload: UserSchema) -> UserSchema:  # noqa
+@router.post("/")
+def create(request: WSGIRequest, payload: UserSchema) -> dict[str, str]:  # noqa
     """Create a new user."""
     user = CustomUser.objects.create_user(
         **payload.dict(exclude_unset=True, exclude_none=True),
     )
-    return UserSchema.from_orm(user)
+
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        "user": user.username,
+        "email": user.email,
+        "full_name": user.full_name,
+        "refresh": str(refresh),
+        "access": str(refresh.access_token),
+    }
 
 
 @router.get("/", response=UserSchema, auth=JWTAuth())
